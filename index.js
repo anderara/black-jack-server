f21e01a1c37ec1f75587374e26eb018d4fa4dc49
AllanRaf <rafale.uk@googlemail.com>
2019-10-08 18:40:33 +0200

    Add route to create gameRoomName

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 //black-jack-server/index.js
 //const User = require('./user/model')
 
-const Cards = require('./cards/model')
+//const Cards = require('./cards/model')
 const bodyParser = require('body-parser')
 const authRouter = require('./auth/router')
 const cors = require('cors')
 
 
 const playerRouter = require('./player/router')
+const cardsRouter = require('./cards/router')
 
 
 const express = require('express')
@@ -23,5 +24,6 @@ app.use(parserMiddleware)
 
 app.use(authRouter)
 app.use(playerRouter)
+app.use(cardsRouter)
 
 app.listen(port, console.log(`listening on port: ${port}`))
